e77280d14588be4b2f5ef02cee27abc0a764f8dd
saasbook <saasbook@saasbook.(none)>
2012-03-08 21:43:23 -0800

    HW2 Part 3 Requirements Met

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,12 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+      if (params["sort"])
+        @movies = Movie.order(params["sort"])
+        flash[:hilite] = params["sort"]
+      else
+        @movies= Movie.all
+      end
   end
 
   def new
